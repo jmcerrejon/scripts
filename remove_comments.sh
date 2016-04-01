ef1c3239379f8c718fcc3287d219ab0eb6b9061a
Jose Cerrejon <ulysess@gmail.com>
2016-04-01 12:35:22 +0200

    Minor fix with // for js

--- remove_comments.sh
+++ remove_comments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find . -type f ! -path "./widgets/*" -regex ".*\.\(js\|html\)" | while read file; do
         ORIG_DATE=$(stat $file | grep ^Mod | awk '{print $2,$3,$4}')
-        sed -i 's/\/.*//;/^$/d;s/<!--.*//' $file
+        sed -i 's/\/\/.*//;/^$/d;s/<!--.*//' $file
         touch -d "${ORIG_DATE}" $file
 done
 
