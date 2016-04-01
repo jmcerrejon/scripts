fb54767ec26471c1bbf76f67d4336efa9504c522
Jose Cerrejon <ulysess@gmail.com>
2016-04-01 11:47:34 +0200

    Update remove_comments.sh

--- remove_comments.sh
+++ remove_comments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find . -type f ! -path "./widgets/*" -regex ".*\.\(js\|html\)" | while read file; do
-        ORIG_DATE=$(stat $file | grep Modificación | awk '{print $2,$3,$4}')
+        ORIG_DATE=$(stat $file | grep ^Mod | awk '{print $2,$3,$4}')
         sed -i 's/\/.*//;/^$/d;s/<!--.*//' $file
         touch -d "${ORIG_DATE}" $file
 done
